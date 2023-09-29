ffe9c17ee0d02d1a85c7646848bce30700bcfb04
robertsivanovs <robertss.ivanovs@gmail.com>
2023-09-29 14:44:06 +0300

    Create migrations for orders table

---
+++ database/migrations/2023_09_29_111834_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_name')->nullable();
+            $table->string('client_phone')->nullable();
+            $table->string('tree_type')->nullable();
+            $table->string('tree_amount')->nullable();
+            $table->string('tree_size')->nullable();
+            $table->string('price')->nullable();
+            $table->string('delivery')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
